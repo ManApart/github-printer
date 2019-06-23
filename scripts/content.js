a8a7ac3543dc044cba33f3617d7c11ba164089d2
ManApart <raustinkucera@gmail.com>
2019-06-23 09:51:47 -0400

    read data from selected cards

--- scripts/content.js
+++ scripts/content.js
@@ -1,11 +1,35 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     console.log("something happening from the extension");
-    var data = request.data || {};
 
-    var linksList = document.querySelectorAll('a');
-    [].forEach.call(linksList, function (header) {
-        header.innerHTML = request.data;
-    });
+    activeCardDivs = getActiveCards()
+    cardProps = activeCardDivs.map(getCardProperties)
+    console.log(cardProps)
+
     sendResponse({ success: true });
 });
 
+getActiveCards = function () {
+    return Array.prototype.slice.call(document.getElementsByClassName('zhc-issue-card--multi-active'))
+}
+
+getCardProperties = function (cardHtml) {
+    card = {}
+    card.title = getInnerText(cardHtml.getElementsByClassName('zhc-issue-card__issue-title')[0])
+    card.number = getInnerText(cardHtml.getElementsByClassName('zhc-issue-card__issue-number')[0])
+    card.repoName = getInnerText(cardHtml.getElementsByClassName('zhc-issue-card__repo-name')[0])
+    card.estimate = getInnerText(cardHtml.getElementsByClassName('zhc-badge__value')[0])
+    card.sprint = getInnerText(cardHtml.getElementsByClassName('zhc-issue-card__milestone__title')[0])
+    card.epic = getInnerText(cardHtml.getElementsByClassName('zhc-issue-card__epics__title')[0])
+    card.feature = getInnerText(cardHtml.getElementsByClassName('zhc-issue-card__release-title')[0])
+
+    card.labels = Array.prototype.slice.call(cardHtml.getElementsByClassName('zhc-label')).map(getInnerText)
+
+    return card
+}
+
+getInnerText = function (div) {
+    if (div) {
+        return div.innerText
+    }
+    return ""
+}
