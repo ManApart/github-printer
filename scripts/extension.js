9f1af695d3acd180e1cc4562c93146116f8c1969
ManApart <raustinkucera@gmail.com>
2019-06-23 08:49:47 -0400

    content gets message

---
+++ scripts/extension.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var button = document.getElementById('changelinks');
+    button.addEventListener('click', function () {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {}, function (response) {
+                console.log('success');
+            });
+        });
+    });
+});
+
